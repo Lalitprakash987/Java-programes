5abec099135ca124729896782b954600d09262b6
Lalitprakash987 <lalitprakash987@gmail.com>
2022-05-31 11:16:01 +0530

    my java programes

---
+++ Main.java
@@ -0,0 +1,36 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("my name is java");
+
+
+        //new string
+        String lalit=new String("lalit prakash fatehpuriya");
+        System.out.println(lalit);
+
+
+        user of Scanner(user input)
+      //  import java.util.Scanner;
+        System.out.println("Enter first number ");
+        Scanner a= new Scanner(System.in);
+        int b=a.nextInt();
+        System.out.println("Enter second number");
+        Scanner c= new Scanner(System.in);
+        int d=c.nextInt();
+        int sum;
+        sum=b+d;
+        System.out.println("sum of two numers   " + sum );
+
+        //Make a jarvis with help of string and Scanner class
+        System.out.println("hello good morng");
+        System.out.println("what is your name ");
+        Sacnner sc =new Scanner(System.in)
+        
+
+
+
+
+    }
+}
